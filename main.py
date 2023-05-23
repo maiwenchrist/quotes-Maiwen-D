65d513580f588ae2ff68ee6500a51773a18cd375
LauraDon <129770274+LauraDon@users.noreply.github.com>
2023-05-23 13:12:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ def main():
 
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
+        elif choice == "5":
+    count = int(input("Enter the number of quotes to display: "))
+    display_quotes(quotes, count)
         elif choice == "3":
             print("Good bye...")
         elif choice == "4":
