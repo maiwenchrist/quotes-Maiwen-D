e803725ace0aad3e291f11c9c068961565afa553
LauraDon <129770274+LauraDon@users.noreply.github.com>
2023-05-23 12:59:51 +0200

    main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def menu():
     print("1. Random quote")
     print("2. All quotes")
     print("3. Exit")
+    print("4. Add quotes")
 
 
 def main():
@@ -21,6 +22,8 @@ def main():
             view_quotes(quotes)
         elif choice == "3":
             print("Good bye...")
+        elif choice == "4":
+            add_quote(quotes, "quotes.txt")
             break
         else:
             print("Invalid input")
